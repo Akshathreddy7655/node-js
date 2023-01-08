fe4e2e8bb7e0c511f4d54d1263ebb64e63ce8791
Sridhar <vaddepelli.sridhar@wohlig.com>
2023-01-08 10:22:25 +0530

    added the dots

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Carrier Bridge');
+  res.send('Hello Carrier Bridge.......');
 });
 
 app.listen(PORT, HOST, () => {
