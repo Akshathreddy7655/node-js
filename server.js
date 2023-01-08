387694a6d4824aabf7c0538cd5e736e1f738bd19
Sridhar <sridhar@abc.com>
2023-01-08 11:49:32 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Carrier Bridge.......AND..HOW WAS THE DAy');
+  res.send('Hello Carrier Bridge.......AND..HOW WAS THE DAY.....HELLO PROJECT HAS BEEN COMPELETED');
 });
 
 app.listen(PORT, HOST, () => {
